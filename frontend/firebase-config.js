d83f6df0b64e26a29c3f3ff84fb98a6f24bedb57
tasha <110906632+tasha404@users.noreply.github.com>
2026-02-09 01:53:35 +0800

    Update firebase-config.js

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// We added these imports for the Database (Firestore)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { 
     getFirestore, collection, doc, addDoc, updateDoc, deleteDoc, 
     onSnapshot, query, orderBy, serverTimestamp, writeBatch 
-} from "firebase/firestore";
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
